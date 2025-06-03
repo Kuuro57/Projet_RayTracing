7b219dbc1b36493b0cea43a26fe77f26d4966c80
Léo Fontaine <leo.fontaine4@orange.fr>
2025-06-03 15:43:49 +0200

    modif

--- tracé_de_rayon/raytracer/client/LancerRaytracer.java
+++ tracé_de_rayon/raytracer/client/LancerRaytracer.java
@@ -18,18 +18,22 @@ public class LancerRaytracer {
 
     public static void main(String args[]) {
 
-        // On récupère l'adresse du serveur et le port si ils sont dans la ligne de commande
+        // On récupère l'adresse du serveur et le port si ils sont dans la ligne de
+        // commande
         // Sinon, on attribut des valeurs par défaut
         String serveur = "localhost";
         int port = 1099;
 
         // On récupère l'adresse de l'annuaire en paramètre
-        if (args.length > 0){ serveur = args[0]; }
+        if (args.length > 0) {
+            serveur = args[0];
+        }
 
         // On récupère le port de l'annuaire en paramètre
-        if (args.length > 1){ port = Integer.parseInt(args[1]); }
+        if (args.length > 1) {
+            port = Integer.parseInt(args[1]);
+        }
 
-        
         try {
 
             // Le fichier de description de la scène si pas fournie
@@ -43,12 +47,13 @@ public class LancerRaytracer {
                 fichier_description = args[2];
                 if (args.length > 2) {
                     largeur = Integer.parseInt(args[3]);
-                    if (args.length > 3) hauteur = Integer.parseInt(args[4]); 
+                    if (args.length > 3)
+                        hauteur = Integer.parseInt(args[4]);
                 }
 
-            } 
-            else { System.out.println(aide); }
-
+            } else {
+                System.out.println(aide);
+            }
 
             // Création d'une fenêtre
             Disp disp = new Disp("Raytracer", largeur, hauteur);
@@ -64,29 +69,29 @@ public class LancerRaytracer {
             int x0 = 0, y0 = 0;
             int l = largeur, h = hauteur;
 
-
             int div = 8;
-            if(args.length > 4){ div = Integer.parseInt(args[5]); }
-
+            if (args.length > 4) {
+                div = Integer.parseInt(args[5]);
+            }
 
             // On récupère l'annuaire local
-            Registry reg = LocateRegistry.getRegistry(port);
+            Registry reg = LocateRegistry.getRegistry(serveur, port);
 
             // On exporte l'objet distant
             SceneInterface stub = (SceneInterface) UnicastRemoteObject.exportObject(scene, 0);
 
             // On récupère le service du distributeur de noeuds
             ServiceDistributeur distrib = (ServiceDistributeur) reg.lookup("distributeur");
-            
+
             // On calcul l'image avec le noeud
             Instant debut = Instant.now();
-            
-            for(int i=0;i<div;i++){
-                for(int j=0;j<div;j++){
+
+            for (int i = 0; i < div; i++) {
+                for (int j = 0; j < div; j++) {
                     NoeudInterface noeud = (NoeudInterface) distrib.getNoeud();
-                    Image image = noeud.calculer(scene,j*l/div, i*h/div, l/div, h/div);
-                    //Image image = scene.compute(j*l/div, i*h/div, l/div, h/div);
-                    disp.setImage(image, j*l/div, i*h/div);
+                    Image image = noeud.calculer(scene, j * l / div, i * h / div, l / div, h / div);
+                    // Image image = scene.compute(j*l/div, i*h/div, l/div, h/div);
+                    disp.setImage(image, j * l / div, i * h / div);
                 }
             }
 
@@ -94,17 +99,11 @@ public class LancerRaytracer {
             long duree = Duration.between(debut, fin).toMillis();
             System.out.println("Image calculée en :" + duree + " ms");
 
-
-
-
-        } 
-        catch (RemoteException e) {
+        } catch (RemoteException e) {
             System.err.println("Erreur lors de la création du serveur RMI : " + e.getMessage());
-        }
-        catch (NotBoundException e) {
+        } catch (NotBoundException e) {
             System.err.println("Erreur : " + e.getMessage());
-        }
-        catch (ServerNotActiveException e) {
+        } catch (ServerNotActiveException e) {
             System.err.println("Erreur : " + e.getMessage());
         }
     }
---
+++ tracé_de_rayon/raytracer/noeuds/MainNoeud.java
@@ -0,0 +1,49 @@
+import java.rmi.server.ServerNotActiveException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.NotBoundException;
+import java.rmi.AccessException;
+
+public class MainNoeud {
+
+    public static void main(String[] args) {
+        // on récupère l'adresse du serveur et le port si ils sont dans la ligne de
+        // commande
+        // si non, on attribut des valeurs par défaut
+        String serveur = "localhost";
+        int port = 1099;
+
+        if (args.length > 0) {
+            serveur = args[0]; // on récupère l'adresse de l'annuaire en paramètre
+        }
+        if (args.length > 1) {
+            port = Integer.parseInt(args[1]); // on récupère le port de l'annuaire en paramètre
+        }
+
+        try {
+            /* Création du noeud */
+            Noeud noeud = new Noeud();
+
+            /* Exporter le noeud */
+            NoeudInterface rd = (NoeudInterface) UnicastRemoteObject.exportObject(noeud, 0);
+
+            /* Récupération de l'annuaire */
+            Registry reg = LocateRegistry.getRegistry(serveur, port);
+
+            // On récupère le distributeur de noeuds
+            ServiceDistributeur distributeur = (ServiceDistributeur) reg.lookup("distributeur");
+
+            // On ajoute le noeud au distributeur
+            distributeur.ajouterNoeud(rd);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (ServerNotActiveException e) {
+            e.printStackTrace();
+        }
+    }
+}
